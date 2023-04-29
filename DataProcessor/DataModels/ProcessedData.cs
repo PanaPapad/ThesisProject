997a98b719bafa97d2c5a20dcf933ad373666f5e
PanaPapad <126814800+PanaPapad@users.noreply.github.com>
2023-04-29 22:16:11 +0300

    Created Data Model classes

--- DataProcessor/DataModels/ProcessedData.cs
+++ DataProcessor/DataModels/ProcessedData.cs
@@ -1,14 +1,14 @@
-//This class defines a RawData record in the IDS database.
 
 using System.ComponentModel.DataAnnotations.Schema;
 
-[Table("RawData")]
-public class RawData{
+[Table("ProcessedData")]
+public class ProcessedData{
     public long Id { get; set; }
+    public long RawDataId { get; set; }
     public DateTime InsertDate { get; set; }
     public byte[] Data { get; set; }
 
-    public RawData(DateTime insertDate, byte[] data){
+    public ProcessedData(DateTime insertDate, byte[] data){
         InsertDate = insertDate;
         Data = data;
     }
--- DataProcessor/DataModels/ResultsData.cs
+++ DataProcessor/DataModels/ResultsData.cs
@@ -1,14 +1,13 @@
-//This class defines a RawData record in the IDS database.
-
 using System.ComponentModel.DataAnnotations.Schema;
 
-[Table("RawData")]
-public class RawData{
+[Table("ResultsData")]
+public class ResultsData{
     public long Id { get; set; }
+    public long ProcessedDataId { get; set; }
     public DateTime InsertDate { get; set; }
     public byte[] Data { get; set; }
 
-    public RawData(DateTime insertDate, byte[] data){
+    public ResultsData(DateTime insertDate, byte[] data){
         InsertDate = insertDate;
         Data = data;
     }
